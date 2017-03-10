1e2e9beef2f6a569a17aa07c01c40a1e328866b1
Roel Van Rossen <Roel Van Rossen>
2017-03-10 12:41:59 +0100

    es5 bestand er bij in gezet

---
+++ labo 3/notes/scripts/app.es5.js
@@ -0,0 +1,37 @@
+function CardApp() {
+    this.buttonAddNote = document.getElementById("btnAddNote");
+    this.notesContainer = document.querySelector(".notes");
+    this.noteInput = document.querySelector("#txtAddNote");
+    
+    this.buttonAddNote.addEventListener("click", this.addNote.bind(this));
+}
+
+CardApp.prototype.resetForm = function(){
+    this.noteInput.value = "";
+    this.noteInput.focus();
+}
+
+CardApp.prototype.addNote = function(e){
+    let newNote = document.createElement("div");
+    newNote.setAttribute("class", "card");
+    newNote.innerHTML = `<p> ${this.noteInput.value} </p>`;
+    
+    let noteLink = document.createElement("a");
+    noteLink.setAttribute("class", "card-remove");
+    noteLink.innerHTML = "Remove";
+    noteLink.setAttribute("href", "#");
+    noteLink.addEventListener("click", this.removeNote.bind(this));
+    
+    newNote.appendChild(noteLink);
+    
+    this.notesContainer.appendChild(newNote);
+    this.resetForm;
+}
+
+CardApp.prototype.removeNote = function(e){
+    let noteToRemove = e.target.parentElement;
+    this.notesContainer.removeChild(noteToRemove);
+    e.preventDefault();
+}
+
+let myApp = new CardApp();
